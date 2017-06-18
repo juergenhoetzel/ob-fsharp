65ec2b626ac55313d8a04e746940370f615fed1e
Juergen Hoetzel <juergen@archlinux.org>
2017-06-18 16:29:39 +0200

    Update to fsharp-mode 1.9.8

--- ob-fsharp-pkg.el
+++ ob-fsharp-pkg.el
@@ -1,3 +1,3 @@
 (define-package "ob-fsharp" "0.1" "Org-Babel F#"
-  '((fsharp-mode "1.9.7")
+  '((fsharp-mode "1.9.8")
     (emacs "25")))
--- ob-fsharp.el
+++ ob-fsharp.el
@@ -4,7 +4,7 @@
 ;; Author: Jürgen Hötzel <juergen@archlinux.org>
 ;; Url: https://github.com/juergenhoetzel/ob-fsharp
 ;; Version: 0.1
-;; Package-Requires: ((emacs "25") (fsharp-mode "1.9.7"))
+;; Package-Requires: ((emacs "25") (fsharp-mode "1.9.8"))
 ;; Keywords: literate programming, reproducible research
 
 ;; This program is free software; you can redistribute it and/or modify
